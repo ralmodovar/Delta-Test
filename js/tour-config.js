15b0d81fb31438d524b1b9343629200d35fb9a19
ralmodovar <60530562+ralmodovar@users.noreply.github.com>
2026-07-23 16:46:40 -0700

    Update tour-config.js

--- js/tour-config.js
+++ js/tour-config.js
@@ -1,6 +1,6 @@
 window.TOUR_CONFIG = {
   project: {
-    title: "Project Horizon",
+    title: "Delta Test",
     description: "Interactive architectural panorama tour."
   },
 
@@ -30,7 +30,7 @@ window.TOUR_CONFIG = {
       title: "Scene 2",
       panorama: "panoramas/scene-2.jpg",
       thumbnail: "thumbs/scene-2.jpg",
-      initialView: { pitch: 0, yaw: 180, hfov: 95 },
+      initialView: { pitch: 0, yaw: 0, hfov: 95 },
       hotspots: [
         {
           pitch: -5,
@@ -39,6 +39,84 @@ window.TOUR_CONFIG = {
           targetPitch: 0,
           targetYaw: 35,
           label: "Return to Scene 1"
+        },
+        {
+          pitch: -5,
+          yaw: 35,
+          targetScene: "scene3",
+          targetPitch: 0,
+          targetYaw: -145,
+          label: "Go to Scene 3"
+        }
+      ]
+    },
+
+    {
+      id: "scene3",
+      title: "Scene 3",
+      panorama: "panoramas/scene-3.jpg",
+      thumbnail: "thumbs/scene-3.jpg",
+      initialView: { pitch: 0, yaw: 0, hfov: 95 },
+      hotspots: [
+        {
+          pitch: -5,
+          yaw: -145,
+          targetScene: "scene2",
+          targetPitch: 0,
+          targetYaw: 35,
+          label: "Return to Scene 2"
+        },
+        {
+          pitch: -5,
+          yaw: 35,
+          targetScene: "scene4",
+          targetPitch: 0,
+          targetYaw: -145,
+          label: "Go to Scene 4"
+        }
+      ]
+    },
+
+    {
+      id: "scene4",
+      title: "Scene 4",
+      panorama: "panoramas/scene-4.jpg",
+      thumbnail: "thumbs/scene-4.jpg",
+      initialView: { pitch: 0, yaw: 0, hfov: 95 },
+      hotspots: [
+        {
+          pitch: -5,
+          yaw: -145,
+          targetScene: "scene3",
+          targetPitch: 0,
+          targetYaw: 35,
+          label: "Return to Scene 3"
+        },
+        {
+          pitch: -5,
+          yaw: 35,
+          targetScene: "scene5",
+          targetPitch: 0,
+          targetYaw: -145,
+          label: "Go to Scene 5"
+        }
+      ]
+    },
+
+    {
+      id: "scene5",
+      title: "Scene 5",
+      panorama: "panoramas/scene-5.jpg",
+      thumbnail: "thumbs/scene-5.jpg",
+      initialView: { pitch: 0, yaw: 0, hfov: 95 },
+      hotspots: [
+        {
+          pitch: -5,
+          yaw: -145,
+          targetScene: "scene4",
+          targetPitch: 0,
+          targetYaw: 35,
+          label: "Return to Scene 4"
         }
       ]
     }
